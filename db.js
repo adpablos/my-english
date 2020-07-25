35f4040ffa4b5ff2519ba9e59ee2c977eb5eb1d0
Alejandro de Pablos <adpabloslopez@gmail.com>
2020-07-24 22:21:38 -0500

    Improvements
    Fixing some issues like the CORS including the headers in the response, having a db connection opt object, and including the environment variable for the lambdas, to have them working in AWS with the babel node, to write in the tmp folder.

--- db.js
+++ db.js
@@ -8,9 +8,21 @@ module.exports = connectToDatabase = () => {
     return Promise.resolve();
   }
 
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    autoIndex: false, // Don't build indexes
+    poolSize: 10, // Maintain up to 10 socket connections
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4 // Use IPv4, skip trying IPv6
+  };
+
   console.log('=> using new database connection');
   console.log('=> url: ' + process.env.DB);
-  return mongoose.connect(encodeURI(process.env.DB), {useNewUrlParser: true})
+  return mongoose.connect(encodeURI(process.env.DB), options)
     .then(db => { 
       isConnected = db.connections[0].readyState;
     });
--- handler.js
+++ handler.js
@@ -1,99 +1,124 @@
 'use strict';
 
-require('dotenv').config({ path: './variables.env' });
+require('dotenv').config({path: './variables.env'});
 
 const connectToDatabase = require('./db');
 const Word = require('./models/Word');
 const wr = require('wordreference-api');
 
 module.exports.create = (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false;
+    context.callbackWaitsForEmptyEventLoop = false;
 
-  connectToDatabase()
-    .then(() => {
-      Word.create(JSON.parse(event.body))
-        .then(word => callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(word)
-        }))
-        .catch(err => callback(null, {
-          statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Could not create the word.'
-        }));
-    });
+    connectToDatabase()
+        .then(() => {
+            Word.create(JSON.parse(event.body))
+                .then(word => callback(null, {
+                    statusCode: 200,
+                    headers: {
+                        "Access-Control-Allow-Headers" : "Content-Type",
+                        "Access-Control-Allow-Origin": "https://master.d1v5hx0ohw6l91.amplifyapp.com",
+                        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                    },
+                    body: JSON.stringify(word)
+                }))
+                .catch(err => callback(null, {
+                    statusCode: err.statusCode || 500,
+                    headers: {'Content-Type': 'text/plain'},
+                    body: 'Could not create the word.'
+                }));
+        });
 };
 
 module.exports.getOne = (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false;
+    context.callbackWaitsForEmptyEventLoop = false;
 
-  connectToDatabase()
-    .then(() => {
-      Word.findById(event.pathParameters.id)
-        .then(word => callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(word)
-        }))
-        .catch(err => callback(null, {
-          statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Could not fetch the word.'
-        }));
-    });
+    connectToDatabase()
+        .then(() => {
+            Word.findById(event.pathParameters.id)
+                .then(word => callback(null, {
+                    statusCode: 200,
+                    headers: {
+                        "Access-Control-Allow-Headers": "Content-Type",
+                        "Access-Control-Allow-Origin": "https://master.d1v5hx0ohw6l91.amplifyapp.com",
+                        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                    },
+                    body: JSON.stringify(word)
+                }))
+                .catch(err => callback(null, {
+                    statusCode: err.statusCode || 500,
+                    headers: {'Content-Type': 'text/plain'},
+                    body: 'Could not fetch the word.'
+                }));
+        });
 };
 
 module.exports.getAll = (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false;
+    context.callbackWaitsForEmptyEventLoop = false;
 
-  connectToDatabase()
-    .then(() => {
-      Word.find()
-        .then(words => callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(words)
-        }))
-        .catch(err => callback(null, {
-          statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Could not fetch the words.'
-        }))
-    });
+    connectToDatabase()
+        .then(() => {
+            Word.find()
+                .then(words => callback(null, {
+                    statusCode: 200,
+                    headers: {
+                        "Access-Control-Allow-Headers" : "Content-Type",
+                        "Access-Control-Allow-Origin": "https://master.d1v5hx0ohw6l91.amplifyapp.com",
+                        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                    },
+                    body: JSON.stringify(words)
+                }))
+                .catch(err => callback(null, {
+                    statusCode: err.statusCode || 500,
+                    headers: {'Content-Type': 'text/plain'},
+                    body: 'Could not fetch the words.'
+                }))
+        });
 };
 
 module.exports.update = (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false;
+    context.callbackWaitsForEmptyEventLoop = false;
 
-  connectToDatabase()
-    .then(() => {
-      Word.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), { new: true })
-        .then(word => callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(word)
-        }))
-        .catch(err => callback(null, {
-          statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Could not fetch the words.'
-        }));
-    });
+    connectToDatabase()
+        .then(() => {
+            Word.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), {new: true})
+                .then(word => callback(null, {
+                    statusCode: 200,
+                    headers: {
+                        "Access-Control-Allow-Headers" : "Content-Type",
+                        "Access-Control-Allow-Origin": "https://master.d1v5hx0ohw6l91.amplifyapp.com",
+                        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                    },
+                    body: JSON.stringify(word)
+                }))
+                .catch(err => callback(null, {
+                    statusCode: err.statusCode || 500,
+                    headers: {'Content-Type': 'text/plain'},
+                    body: 'Could not fetch the words.'
+                }));
+        });
 };
 
 module.exports.delete = (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false;
+    context.callbackWaitsForEmptyEventLoop = false;
 
-  connectToDatabase()
-    .then(() => {
-      Word.findByIdAndRemove(event.pathParameters.id)
-        .then(word => callback(null, {
-          statusCode: 200,
-          body: JSON.stringify({ message: 'Removed word with id: ' + word._id, word: word })
-        }))
-        .catch(err => callback(null, {
-          statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Could not fetch the words.'
-        }));
-    });
+    connectToDatabase()
+        .then(() => {
+            Word.findByIdAndRemove(event.pathParameters.id)
+                .then(word => callback(null, {
+                    statusCode: 200,
+                    headers: {
+                        "Access-Control-Allow-Headers" : "Content-Type",
+                        "Access-Control-Allow-Origin": "https://master.d1v5hx0ohw6l91.amplifyapp.com",
+                        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                    },
+                    body: JSON.stringify({message: 'Removed word with id: ' + word._id, word: word})
+                }))
+                .catch(err => callback(null, {
+                    statusCode: err.statusCode || 500,
+                    headers: {'Content-Type': 'text/plain'},
+                    body: 'Could not fetch the words.'
+                }));
+        });
 };
 
 module.exports.wrSearch = (event, context, callback) => {
@@ -102,9 +127,17 @@ module.exports.wrSearch = (event, context, callback) => {
     connectToDatabase()
         .then(() => {
             let body = JSON.parse(event.body);
-            wr(body.word,body.from,body.to).then((result)=> callback(null, {
+            wr(body.word, body.from, body.to).then((result) => callback(null, {
                 statusCode: 200,
-                body: JSON.stringify({ message: 'Translation of ' + body.word + ' from ' + body.from + ' to ' + body.to, result: result })
+                headers: {
+                    "Access-Control-Allow-Headers" : "Content-Type",
+                    "Access-Control-Allow-Origin": "https://master.d1v5hx0ohw6l91.amplifyapp.com",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                },
+                body: JSON.stringify({
+                    message: 'Translation of ' + body.word + ' from ' + body.from + ' to ' + body.to,
+                    result: result
+                })
             }));
         });
 };
